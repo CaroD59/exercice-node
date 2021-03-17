178a191cb0d4b5a176042fccbdc1f7b6fc633d3b
CaroD59 <dorchies.c@gmail.com>
2021-03-17 12:29:45 +0100

    Create files

---
+++ index.js
@@ -0,0 +1,8 @@
+const myInfos = require("./information.js");
+const cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text: "Hello I'm ${myInfo.name} from ${myInfo.campus} :) !",
+    e: "oO",
+    T: "U "
+}));
---
+++ information.js
@@ -0,0 +1,6 @@
+const myInfos = {
+	name: "Caroline",
+	campus: "Lille",
+  };
+  
+  module.exports = myInfos;
